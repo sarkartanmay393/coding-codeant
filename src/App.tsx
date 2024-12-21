2e763c185e558d5377c486fa3f9662eb9b4e5d8c
Tanmay Sarkar <tanmay.sarkar@nirmata.com>
2024-12-21 17:19:40 +0530

    Refactor import statements in App.tsx for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import './global.css';
+import "./global.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignInPage from "./components/SignInPage";
 import styled from "@emotion/styled";
-import Dashboard from './components/Dashboard';
+import Dashboard from "./components/Dashboard";
 
 const Layout = styled.div`
   height: 100vh;
