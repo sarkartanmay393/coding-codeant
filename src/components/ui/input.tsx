a7a990bd5148566cc02985444652e4cb215dc2a3
Tanmay Sarkar <tanmay.sarkar@nirmata.com>
2024-12-22 15:19:32 +0530

    Update SearchInput component to use max-w-sm

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -25,13 +25,13 @@ const SearchInput = React.forwardRef<
   React.ComponentProps<"input">
 >(({ className, type, ...props }, ref) => {
   return (
-    <div className="flex items-center gap-2 h-9 w-full max-w-md rounded-lg border border-input bg-transparent text-base shadow-sm transition-colors">
+    <div className="flex items-center gap-2 h-9 w-full max-w-sm rounded-lg border border-input bg-transparent text-base shadow-sm transition-colors">
       <div className="pl-3 py-1">
         <SearchIcon className="h-4 w-4" />
       </div>
       <input
         type={type}
-        className={cn("rounded-lg flex h-full w-full max-w-md pr-1 py-1 ring-0 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm", className)}
+        className={cn("rounded-lg flex h-full w-full max-w-sm pr-1 py-1 ring-0 file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm", className)}
         ref={ref}
         {...props}
       />
